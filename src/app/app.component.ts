7860be75b6f2141ae211c1435bd22204a0843c54
Mohd Faisal <mohd@thetrustsearch.com>
2020-06-20 01:33:53 +0530

    add event bindings to inputs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app works!';
+  length: Number = 0;
+  password: String = "";
+  useNumbers: Boolean = false;
+  useLetters: Boolean = false;
+  useSymbols: Boolean = false;
+
+  handleLengthChange(event: any) {
+    const parsedNumber = event.target.value;
+    if (!isNaN(parsedNumber)) {
+      this.length = parsedNumber;
+    }
+  }
+
+  handleUseNumbersChange() {
+    this.useNumbers = !this.useNumbers;
+  }
+
+  handleUseLettersChange() {
+    this.useLetters = !this.useLetters;
+  }
+
+  handleUseSymbolsChange() {
+    this.useSymbols = !this.useSymbols;
+  }
 }
